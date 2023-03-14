ea4956c86c9c2e84511af181c22880649ab2f009
erlingmi <46762317+erlingmi@users.noreply.github.com>
2023-03-14 22:55:05 +0100

    Create demografi.php

---
+++ demografi.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <script src="https://d3js.org/d3.v6.min.js"></script>
+    <script src="my_script.js"></script>
+    <style>
+      #plot {
+        width: 800px;
+        height: 600px;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="plot"></div>
+  </body>
+</html>
